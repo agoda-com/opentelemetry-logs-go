bb256cf30878c590ba6dc34921af56fbcbbc1627
Aleksandr Nekrasov <snake_nas@mail.ru>
2023-08-06 00:06:34 +0700

    release v0.1.2 (#10)
    Co-authored-by: Aleksandr Nekrasov <aleksandr.nekrasov@agoda.com>

--- exporters/otlp/internal/retry/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/agoda-com/opentelemetry-logs-go/internal/retry
-
-go 1.19
-
-require github.com/cenkalti/backoff/v4 v4.2.1
--- version.go
+++
@@ -1,6 +0,0 @@
-package otel
-
-// Version is the current release version of OpenTelemetry Logs in use.
-func Version() string {
-	return "0.1.1"
-}
