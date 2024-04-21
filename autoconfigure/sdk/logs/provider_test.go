cb82be5fd452e5b571fc975b7690d1aaf1a76ca3
Aleksandr Nekrasov <aleksandr.nekrasov@agoda.com>
2024-04-21 15:52:23 +0700

    release/v0.5.0

--- autoconfigure/sdk/logs/provider_test.go
+++ autoconfigure/sdk/logs/provider_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/agoda-com/opentelemetry-logs-go/logs"
 	sdk "github.com/agoda-com/opentelemetry-logs-go/sdk/logs"
 	"github.com/stretchr/testify/assert"
-	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
 	"testing"
 	"time"
 )
